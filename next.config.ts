207d4078a747e895f7573ba77fb78c60537a9c36
Rahul Swain <rahuls.403.dev@gmail.com>
2026-05-28 13:11:53 +0530

    feat: update next.config.ts to enhance server actions and image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,22 @@
 import type { NextConfig } from "next";
-import { hostname } from "os";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100mb",
+    },
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "covers.openlibrary.org" }],
+    remotePatterns: [
+      { protocol: "https", hostname: "covers.openlibrary.org" },
+      {
+        protocol: "https",
+        hostname: "lspfdyhgsrgsxcju.public.blob.vercel-storage.com",
+      },
+    ],
   },
 };
 
